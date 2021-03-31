2e0866c55885bf89e40443c6e5da43c13b13eeab
Max Leiter <maxwell.leiter@gmail.com>
2021-03-31 02:29:14 -0700

    Matomo test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import App from 'next/app'
+import { init } from "@socialgouv/matomo-next";
 
 import '@styles/global.css'
 
+const MATOMO_URL = process.env.MATOMO_URL;
+const MATOMO_SITE_ID = process.env.MATOMO_SITE_ID;
+
 class MyApp extends App {
+  componentDidMount() {
+    if (MATOMO_SITE_ID && MATOMO_URL) {
+      init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
+    }
+  }
+
   render() {
     const { Component, pageProps } = this.props
     return (
