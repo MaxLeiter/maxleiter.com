5efccc5d55082451a0336ccbc4cf92fac45d0332
Max Leiter <maxwell.leiter@gmail.com>
2021-03-31 02:54:47 -0700

    Matomo fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,24 @@
 import React from 'react'
 import App from 'next/app'
-import { init } from "@socialgouv/matomo-next";
-
+import { MatomoProvider, createInstance } from '@datapunt/matomo-tracker-react'
 import '@styles/global.css'
 
-const MATOMO_URL = process.env.MATOMO_URL;
-const MATOMO_SITE_ID = process.env.MATOMO_SITE_ID;
+const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL;
+const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID;
+const instance = createInstance({
+  urlBase: `https://${MATOMO_URL}`,
+  siteId: MATOMO_SITE_ID ? parseInt(MATOMO_SITE_ID) : 1,
+  linkTracking: true
+  // trackerUrl: 'https://LINK.TO.DOMAIN/tracking.php', // optional, default value: `${urlBase}matomo.php`
+  // srcUrl: 'https://LINK.TO.DOMAIN/tracking.js', // optional, default value: `${urlBase}matomo.js`
+})
 
 class MyApp extends App {
-  componentDidMount() {
-    if (MATOMO_SITE_ID && MATOMO_URL) {
-      init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
-    }
-  }
 
   render() {
     const { Component, pageProps } = this.props
     return (
-        <Component {...pageProps} />
+      <MatomoProvider value={instance}><Component {...pageProps} /></MatomoProvider>
     )
   }
 }
