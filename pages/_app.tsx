ee068b6217a08588e6ab793e64fcef39821a838c
Max Leiter <maxwell.leiter@gmail.com>
2021-04-05 16:29:09 -0700

    Fix dupe matomo analytics being reported

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,6 @@
 import React from 'react'
 import App from 'next/app'
-import Router from 'next/router'
 import '@styles/global.css'
-
-Router.events.on("routeChangeStart", url => {
-  console.log("yeah")
-  //@ts-ignore
-  if (window && window._paq) {
-    window._paq.push(["setCustomUrl", url]);
-    window._paq.push(["setDocumentTitle", document.title]);
-    window._paq.push(["trackPageView"]);
-  }
-});
 
 class MyApp extends App {
   componentDidMount() {
