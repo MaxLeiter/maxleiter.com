d58e940310b9177d3e27b9b38995f1f27c639b4f
Max Leiter <maxwell.leiter@gmail.com>
2022-02-17 01:43:44 -0800

    Posts list: FadeIn on render

--- components/posts-list/index.tsx
+++ components/posts-list/index.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import BlockEntry from '@components/entry/block'
 import styles from './posts-list.module.css'
 import type { Post } from '@lib/types'
+import FadeIn from '@components/fade-in'
 
 type Props = {
   posts: Post[]
@@ -22,14 +23,14 @@ const Posts = ({ posts, paginate }: Props) => {
         })
 
         return (
-          <BlockEntry
-            key={`post-item-${post.slug}`}
-            href={`/blog/${post.slug}`}
-            title={post.title}
-            type={date}
-            as={`/blog/${post.slug}`}
-            description={post.description}
-          />
+          <FadeIn key={`post-item-${post.slug}`}>
+            <BlockEntry
+              href={`/blog/${post.slug}`}
+              title={post.title}
+              type={date}
+              as={`/blog/${post.slug}`}
+              description={post.description}
+            /></FadeIn>
         )
       })}
       {paginate && showMore < posts.length && (
