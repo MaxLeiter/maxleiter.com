df3180c0bff7a58e5161e2d7ae7a1ba28fdea7b8
Max Leiter <maxwell.leiter@gmail.com>
2022-02-17 02:22:59 -0800

    Revert "Posts list: FadeIn on render"
    This reverts commit d58e940310b9177d3e27b9b38995f1f27c639b4f.

--- components/posts-list/index.tsx
+++ components/posts-list/index.tsx
@@ -3,7 +3,6 @@ import { useState } from 'react'
 import BlockEntry from '@components/entry/block'
 import styles from './posts-list.module.css'
 import type { Post } from '@lib/types'
-import FadeIn from '@components/fade-in'
 
 type Props = {
   posts: Post[]
@@ -23,14 +22,14 @@ const Posts = ({ posts, paginate }: Props) => {
         })
 
         return (
-          <FadeIn key={`post-item-${post.slug}`}>
-            <BlockEntry
-              href={`/blog/${post.slug}`}
-              title={post.title}
-              type={date}
-              as={`/blog/${post.slug}`}
-              description={post.description}
-            /></FadeIn>
+          <BlockEntry
+            key={`post-item-${post.slug}`}
+            href={`/blog/${post.slug}`}
+            title={post.title}
+            type={date}
+            as={`/blog/${post.slug}`}
+            description={post.description}
+          />
         )
       })}
       {paginate && showMore < posts.length && (
