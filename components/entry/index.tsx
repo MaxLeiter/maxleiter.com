25ca38c046705718898d46fc18e56ef4bd68f84a
Max Leiter <maxwell.leiter@gmail.com>
2022-02-19 17:31:21 -0800

    Remove unnecessary CSS Detected using https://www.jitbit.com/unusedcss/

--- components/entry/index.tsx
+++ components/entry/index.tsx
@@ -1,7 +1,5 @@
 import Badge from '@components/badge'
-import Hovercard from '@components/hovercard'
 import Link from '@components/link'
-import { ReactNode } from 'react'
 import styles from './entry.module.css'
 
 type Props = {
@@ -9,10 +7,8 @@ type Props = {
   title: string
   description: string
   role: string
-  // stars?: number
   years: string[]
   showYears: boolean
-  hovercard?: ReactNode
 }
 
 const Entry = ({
@@ -20,21 +16,14 @@ const Entry = ({
   title,
   description,
   role,
-  // stars = -1,
   years,
   showYears = true,
-  hovercard
 }: Props) => (
   <li className={styles.wrapper}>
     <div className={styles.split}>
-      {hovercard && <Hovercard card={hovercard}>
-        <Link href={href} external={true}>
-          {title}
-        </Link>{' '}
-      </Hovercard>}
-      {!hovercard && <Link href={href} external={true}>
+      <Link href={href} external={true}>
         {title}
-      </Link>}
+      </Link>
       <Badge className={styles.badge}>{role}</Badge>
       {showYears && (
         <Badge>
--- components/projects/index.tsx
+++ components/projects/index.tsx
@@ -18,26 +18,16 @@ const Projects = ({ count = -1, projects, showYears = true }: Props) => {
   return (
     <ul className={styles.container}>
       {sliced.map((e) => {
-        // const yearsText = e.years.length > 1 ? <><b>Years:</b> {e.years[0]} - {e.years[1]}</> : <><b>Year:</b> {e.years[0]}</>
-        // const shouldShowHovercard = e.stars && e.stars > -1 ? true : showYears ? false : true
         return (
           <Entry
             showYears={showYears}
             years={e.years}
-            // stars={e.stars}
             key={e.title}
             href={e.href}
             title={e.title}
             description={e.description}
             role={e.role}
-            // hovercard={shouldShowHovercard && <div className={styles.card}>
-            //   {!showYears && yearsText}
-            //   {e.stars && e.stars > -1 && <div className={styles.stars}><b>GitHub</b> <Star size={16} />: {e.stars}</div>}
-            //   {/* @ts-ignore */}
-            //   {!!e.cardInfo && <div dangerouslySetInnerHTML={{__html: e.cardInfo}}
-            //    />}
-            // </div>} 
-            />
+          />
         )
       })}
       {count > 0 && count < projects.length && (
