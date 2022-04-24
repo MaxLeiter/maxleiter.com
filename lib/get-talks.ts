00769037301f04b4065ef4d97164e145ef78e7fc
Max Leiter <maxwell.leiter@gmail.com>
2022-04-24 16:18:18 -0700

    Improve a few talk notes

--- lib/get-talks.ts
+++ lib/get-talks.ts
@@ -3,9 +3,9 @@ import { Client } from "youtubei";
 
 
 const talks = [
-    { description: 'A great channel for humorous in-depth explanations on N64 hacking and emulations.', url: 'https://www.youtube.com/watch?v=t_rzYnXEQlE' },
+    { description: 'A great channel for humorous in-depth explanations on N64 hacking and emulation.', url: 'https://www.youtube.com/watch?v=t_rzYnXEQlE' },
     { description: 'Lin Clark provides an excellent fun graphical explanation of WASI, the standardized WASM system interface.', url: 'https://www.youtube.com/watch?v=fh9WXPu0hw8' },
-    { description: 'The creator of JSON (although he would argue "discoverer") walks you through many of original ideas and reasonings behind the JSON language. My personal favorite in this list, I think.', url: 'https://www.youtube.com/watch?v=-C-JoyNuQJs' },
+    { description: 'The creator of JSON (although he would argue "discoverer") walks through many of the original ideas and reasonings behind JSON. My personal favorite video in this list, I think.', url: 'https://www.youtube.com/watch?v=-C-JoyNuQJs' },
     { description: 'A high-level overview of the TypeScript compiler, covering type inference, ASTs, the flow graph, and more.', url: 'https://www.youtube.com/watch?v=X8k_4tZ16qU' },
     { description: 'Both an introduction to WebAssembly and clang\'s ability to compile C++ to WASM. Smith walks the viewer through compiling and running clang in the browser. ', url: 'https://www.youtube.com/watch?v=5N4b-rU-OAA' },
     { description: 'Koretskyi goes on a deep-dive into optimizations taken by frameworks in order to achieve maximum performance in Node and V8. Memory allocation, bloom filters, dependency injection; you name it, it\'s covered.', url: 'https://www.youtube.com/watch?v=_VHNTC67NR8' },
@@ -14,10 +14,10 @@ const talks = [
     { description: 'Dylan Beattie imagines if the web was created with an alternate history: no google, no FireFox, no WWW.', url: 'https://www.youtube.com/watch?v=9CSjlZeqKOc' },
     { description: 'Why is Rust a good investment for "future-proof" software?', url: 'https://www.youtube.com/watch?v=A3AdN7U24iU' },
     { description: 'Andrew Kelley demonstrates the flexibility and power of Zig by creating a portable Vulkan program with no libc dependency.', url: 'https://www.youtube.com/watch?v=pq1XqP4-qOo' },
-    { description: 'A brilliant introduction to the BEAM virtual machine and it\'s capabilities. ', url: 'https://www.youtube.com/watch?v=JvBT4XBdoUE' },
+    { description: 'A brilliant introduction to the BEAM virtual machine and its capabilities. ', url: 'https://www.youtube.com/watch?v=JvBT4XBdoUE' },
     { description: 'A talk focusing on the power of the collective, but with amazingly interesting tidbits related to CRISPR and bringing back extinct species.', url: 'https://www.youtube.com/watch?v=xJbXk7NldnM' },
-    { description: 'It\'s frankly terrifying the length to which North Korea has gone to spy on their citizens. Possibly my favorite talk on this page.', url: 'https://www.youtube.com/watch?v=8LGDM9exlZw' },
-    { description: 'Dylan Beattie is the best conference speaker I\'ve seen, and I feel like this is his best talk (it\'s definitely his most popular). In this, Beattie explores why code is beautiful, and he show\'s examples too.', url: 'https://www.youtube.com/watch?v=6avJHaC3C2U' },
+    { description: 'It\'s frankly terrifying the length to which North Korea has gone to spy on their citizens.', url: 'https://www.youtube.com/watch?v=8LGDM9exlZw' },
+    { description: 'Dylan Beattie is the best conference speaker I\'ve seen, and I feel like this is his best talk (it\'s definitely his most popular). In this, Beattie explores why code is beautiful, and he shows examples too.', url: 'https://www.youtube.com/watch?v=6avJHaC3C2U' },
     { description: 'A design-focused talk; Wichary explores the complexity behind typography, language, and a platform as complex as Figma.', url: 'https://www.youtube.com/watch?v=kVD-sjtFoEI' },
 ];
 
