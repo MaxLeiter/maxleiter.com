88ea092462c6f0e8eb3ed765cc0a0eb57a7dfcd5
Max Leiter <maxwell.leiter@gmail.com>
2022-05-15 20:02:02 -0700

    middleware: don't send analytics on certain user agents

--- pages/_middleware.tsx
+++ pages/_middleware.tsx
@@ -7,7 +7,29 @@ export const middleware: NextMiddleware = async (req, event) => {
 		!PUBLIC_FILE.test(pathname) &&
 		!pathname.startsWith("/api")
 
+        const userAgentsToIgnore = [
+            'Googlebot',
+            'Bingbot',
+            'Slackbot',
+            'Slurp',
+            'DuckDuckBot',
+            'Baiduspider',
+            'YandexBot',
+            'facebookexternalhit',
+            'ia_archiver',
+            'Vercel Edge Functions'
+        ]
+
+        const isBot =
+            req.headers &&
+            userAgentsToIgnore.some(ua => req.headers.get('user-agent')?.includes(ua))
+            
     const sendAnalytics = async () => {
+        if (isBot) {
+            console.log('Bot/crawler detected, not sending analytics')
+            return
+        }
+
         const slug = pathname.slice(pathname.indexOf("/")) || "/"
 
         const URL = process.env.NODE_ENV === 'production' ? 'https://maxleiter.com/api/view' : 'http://localhost:3000/api/view'
