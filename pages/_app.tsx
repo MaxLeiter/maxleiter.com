0a63fbfdc2eb4e0df55e173d08a75f8e0bd720a3
Max Leiter <maxwell.leiter@gmail.com>
2022-05-18 14:12:24 -0700

    default to system color scheme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,10 @@ import React from 'react'
 import type { AppProps } from 'next/app'
 
 import '@styles/global.css'
-import { ThemeProvider, useTheme } from 'next-themes'
+import { ThemeProvider } from 'next-themes'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const { systemTheme } = useTheme()
-  return (<ThemeProvider disableTransitionOnChange defaultTheme={systemTheme}>
+  return (<ThemeProvider disableTransitionOnChange enableSystem={true}>
       <Component {...pageProps} />
     </ThemeProvider>)
 }
