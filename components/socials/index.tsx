7247f834f48716cf611ed8a0e2444e23980664b7
Max Leiter <maxwell.leiter@gmail.com>
2022-05-20 23:05:38 -0700

    remove unnecessary screen reader div in tooltip children

--- components/socials/index.tsx
+++ components/socials/index.tsx
@@ -4,19 +4,18 @@ import styles from './socials.module.css'
 import { GitHub, Twitter, LinkedIn, Mail, RSS } from '@components/icons'
 import Tooltip from '@components/tooltip'
 import ThemeSwitcher from '@components/theme-switcher'
-import { useTheme } from 'next-themes'
 
 type SocialButtonProps = {
   href: string
   icon: React.ReactNode
   tooltip: string
 }
+
 const SocialButton = ({ tooltip, href, icon }: SocialButtonProps) => {
   return (
     <Tooltip text={tooltip}>
       <Link href={href} className={styles.icon}>
         {icon}
-        <span className={styles['sr-only']}>{tooltip}</span>
       </Link>
     </Tooltip>
   )
