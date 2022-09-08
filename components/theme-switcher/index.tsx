71f372cd8a6bf3b87be9ecdb8e1218e7064849f8
Max Leiter <max.leiter@vercel.com>
2022-09-07 18:24:17 -0700

    theme-switcher: fix theme flash on load

--- components/theme-switcher/index.tsx
+++ components/theme-switcher/index.tsx
@@ -2,9 +2,19 @@ import { Moon, Sun } from '@components/icons'
 import { useTheme } from 'next-themes'
 import socialStyles from '@components/socials/socials.module.css'
 import Tooltip from '@components/tooltip'
+import { useEffect, useState } from 'react'
 
 const ThemeSwitcher = ({ className = '' }: { className?: string }) => {
   const { theme: activeTheme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
 
   return (
     <Tooltip text={activeTheme === 'light' ? 'Dark mode' : 'Light mode'}>
