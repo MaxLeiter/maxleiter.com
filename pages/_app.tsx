253a7c56332b1247548ae99bd59ba7fd4806c5de
Max Leiter <max.leiter@vercel.com>
2022-10-27 20:30:43 -0700

    @vercel/analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react'
 
 import '@styles/global.css'
 import { ThemeProvider } from 'next-themes'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider disableTransitionOnChange enableSystem={true} >
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <ThemeProvider disableTransitionOnChange enableSystem={true}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+      <Analytics />
+    </>
   )
 }
