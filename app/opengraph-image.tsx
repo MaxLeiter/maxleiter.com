4b2a8e3d27b3b72fb4f50359ee166322022fa193
Max Leiter <max.leiter@vercel.com>
2023-04-01 21:46:11 -0700

    kind of fix OG

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,5 +1,4 @@
 import { ImageResponse } from '@vercel/og'
-import * as htmlparser2 from 'htmlparser2'
 
 export const size = { width: 1200, height: 600 }
 // TODO: update to support alt once nextjs has a solution for params
@@ -11,56 +10,10 @@ export const config = {
 }
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default async function ({
-  params,
-}: {
-  params: {
-    slug: string
-  }
-}): Promise<ImageResponse> {
-  // const post = (await getPosts()).find((p) => p?.slug === params.slug)
-  // instead of getPosts, lets fetch VERCEL_URL/sitemap.xml and parse it with htmlparser2
-  // then we can get the post data from there
-  const post = await fetch(
-    `https://${process.env.VERCEL_URL}/sitemap.xml`
-  ).then((res) => res.text())
-
-  // extract the /blog/{post} data from the sitemap
-  const parser = new htmlparser2.Parser(
-    {
-      onopentag(name, attribs) {
-        if (name === 'loc' && attribs.href.includes('/blog/')) {
-          console.log(attribs.href)
-        }
-      },
-      ontext(text) {
-        console.log('-->', text)
-      },
-      onclosetag(tagname) {
-        if (tagname === 'loc') {
-          console.log('That\'s it?')
-        }
-      },
-    },
-    { decodeEntities: true }
-  )
-  parser.write(post)
-  parser.end()
-
-  if (!post) {
-    return new Response('Not found', { status: 404 })
-  }
-
+export default async function (): Promise<ImageResponse> {
   const fontData = await fetch(
-    new URL('../../../fonts/Inter-Medium.ttf', import.meta.url)
+    new URL('./fonts/Inter-Medium.ttf', import.meta.url)
   ).then((res) => res.arrayBuffer())
-
-  const title = post.title
-  const date = post.date
-
-  if (!title) {
-    return new Response('Missing title', { status: 400 })
-  }
 
   return new ImageResponse(
     (
@@ -96,18 +49,6 @@ export default async function ({
           >
             maxleiter.com
           </span>
-          {date && (
-            <div
-              style={{
-                fontSize: 25,
-                background: 'white',
-                color: 'black',
-                padding: '4px 10px',
-              }}
-            >
-              {date}
-            </div>
-          )}
         </div>
         <div
           style={{
@@ -123,18 +64,7 @@ export default async function ({
             maxWidth: 1000,
           }}
         >
-          {title && (
-            <div
-              style={{
-                fontSize: 65,
-                fontWeight: 900,
-                marginBottom: 40,
-                lineHeight: 1.1,
-              }}
-            >
-              {title}
-            </div>
-          )}
+          Max Leiter&apos;s Website
         </div>
       </div>
     ),
