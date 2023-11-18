c3d4136fc2d084fd2282ab7c265f1fd6a4486861
Max Leiter <max.leiter@vercel.com>
2023-11-18 10:36:33 -0800

    Bump deps

--- app/components/aboutme/index.tsx
+++ app/components/aboutme/index.tsx
@@ -3,17 +3,13 @@ import Link from '@components/link'
 const AboutMe = () => (
   <>
     <p>
-      I'm currently building at{' '}
+      I&apos;m currently building at{' '}
       <Link href="https://vercel.com" external>
         Vercel
       </Link>
       . I&apos;m interested in politics, tech, and building a fast, accessible
       web.
     </p>
-    {/* <p>
-      You can view a list of talks I recommend watching or listening to on{' '}
-      <Link href="/talks">this page</Link>.
-    </p> */}
   </>
 )
 
