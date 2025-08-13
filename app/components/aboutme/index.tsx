b0802fd18ace24421e7ea0d1aa7ffca18bdb537a
Max Leiter <max.leiter@vercel.com>
2025-08-12 19:30:50 -0700

    v0.app

--- app/components/aboutme/index.tsx
+++ app/components/aboutme/index.tsx
@@ -3,7 +3,11 @@ import Link from '@components/link'
 const AboutMe = () => (
   <>
     <p>
-      I&apos;m currently building at{' '}
+      I&apos;m currently building{' '}
+      <Link href="https://v0.app" style={{ fontFamily: 'monospace' }} external>
+        v0.app
+      </Link>{' '}
+      at{' '}
       <Link href="https://vercel.com" external>
         Vercel
       </Link>
