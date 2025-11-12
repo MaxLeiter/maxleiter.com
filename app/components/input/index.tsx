ea495bb3f423b80c9180dbccfdd755c0e1e928b4
Max Leiter <max.leiter@vercel.com>
2025-11-11 21:10:26 -0800

    rm files

--- app/components/input/index.tsx
+++
@@ -1,7 +0,0 @@
-import styles from './input.module.css'
-
-const Input = (props: React.InputHTMLAttributes<HTMLInputElement>) => {
-  return <input type="text" className={styles.input} {...props} />
-}
-
-export default Input
--- app/lib/get-blog-post-content.ts
+++
@@ -1,8 +0,0 @@
-'use server'
-
-import { getPost } from './get-posts'
-
-export async function getBlogPostContent(slug: string): Promise<string | null> {
-  const post = await getPost(slug)
-  return post?.body || null
-}
